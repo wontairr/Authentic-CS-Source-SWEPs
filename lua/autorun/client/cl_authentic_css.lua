e4903770bc392955125987becf5d0ce91e1ef28d
Wontairr <nathanaelcampo@outlook.com>
2025-08-04 11:38:57 -0500

    Fix CSS Knife Icon

--- lua/autorun/client/cl_authentic_css.lua
+++ lua/autorun/client/cl_authentic_css.lua
@@ -127,14 +127,14 @@ CSSSmokeColor = Color(70,70,70,255)
 CSSSmokeGrenades = {}
 
 local PLAYER = FindMetaTable("Player")
-killicon.AddFont("weapon_knife","CSKillIcons","J",Color(255,80,0),0.2)
+killicon.AddFont("weapon_knife","CSKillIcons","j",Color(255,80,0),0.2)
 
 hook.Add("Initialize","CSSWeapons_KillIcons",function()
     for _,weapon in ipairs(weapons.GetList()) do
         if not weapon.Base then continue end
         if string.find(weapon.Base,"css_weapon_") != nil and not killicon.Exists(weapon.ClassName) then
             if weapon.ClassName == "weapon_knife" then
-                killicon.AddFont("weapon_knife","CSKillIcons","J",Color(255,80,0),0.2)
+                killicon.AddFont("weapon_knife","CSKillIcons","j",Color(255,80,0),0.2)
                 continue
             end
             killicon.AddFont(weapon.ClassName,"CSKillIcons",weapon.IconLetter,Color(255,80,0),0.2)
